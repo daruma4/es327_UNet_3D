0bb41139c39893a94d7763a7675e85141e7cc433
Gabriel Bac <gabibac8@gmail.com>
2024-01-30 17:43:58 +0000

    Added iou_loss loss function to unet.py

--- unet.py
+++ unet.py
@@ -144,6 +144,9 @@ class unet_model:
         y_pred_f = K.flatten(y_pred)
         intersection = K.sum(y_true_f * y_pred_f)
         return (2.0 * intersection + 1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1)
+    
+    def j_iou_loss(self, y_true, y_pred):
+        return 1 - self.j_iou(y_true, y_pred)
 
     def j_iou(self, y_true, y_pred):
         y_true_f = K.flatten(y_true)
