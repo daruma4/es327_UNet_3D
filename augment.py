4761e9ff6a6d1d563346c5214e21799fc65c1120
Gabriel Bac <gabibac8@gmail.com>
2024-02-01 19:05:52 +0000

    augment.py - added 3 transforms. Could make this an arguement and clean up code.

--- augment.py
+++ augment.py
@@ -16,14 +16,31 @@ def do_albumentations(transform, img_list, mask_list, slice_count):
         for i in range(slice_count):
             if i is 0:
                 slice_dict[f"image"] = cv2.cvtColor(img_list[idx][i], cv2.COLOR_BGR2RGB) ### first image has to be "image", no idx
-                slice_dict[f"mask"] = cv2.cvtColor(mask_list[idx][i], cv2.COLOR_BGR2RGB) ### same with mask
+                slice_dict[f"mask"] = cv2.cvtColor(mask_list[idx][i], cv2.COLOR_BGR2RGB) ### same with mask. An idiosyncrasy of albumentations lib.
                 continue
             slice_dict[f"image{i}"] = cv2.cvtColor(img_list[idx][i], cv2.COLOR_BGR2RGB) ###
             slice_dict[f"mask{i}"] = cv2.cvtColor(mask_list[idx][i], cv2.COLOR_BGR2RGB) ###
 
         transformed = transform(**slice_dict)
+        transformed2 = transform(**slice_dict)
+        transformed3 = transform(**slice_dict)
         
+        #Could make this nicer...
         for i in transformed:
+            if "image" in i:
+                augemented_raw_list.append(transformed[i])
+            elif "mask" in i:
+                augmented_mask_list.append(transformed[i])
+            else:
+                raise Exception(f"ERROR: Could not identify key in transformed dict. {i} not in transformed.")
+        for i in transformed2:
+            if "image" in i:
+                augemented_raw_list.append(transformed[i])
+            elif "mask" in i:
+                augmented_mask_list.append(transformed[i])
+            else:
+                raise Exception(f"ERROR: Could not identify key in transformed dict. {i} not in transformed.")
+        for i in transformed3:
             if "image" in i:
                 augemented_raw_list.append(transformed[i])
             elif "mask" in i:
